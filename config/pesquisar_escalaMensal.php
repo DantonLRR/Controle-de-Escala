e52f533e07c3c2c0aa794f310b46f4b01dd57a41
Danton Lucas Ribeiro Rodrigues <Dantonlucasrr@gmail.com>
2023-11-19 17:28:38 -0300

    exibição correta de imagem de carregamento

--- config/pesquisar_escalaMensal.php
+++ config/pesquisar_escalaMensal.php
@@ -17,7 +17,7 @@ $verifica = new verifica();
 // echo $dataSelecionadaNoFiltro;
 // echo"<br>".$loja;
 $verificaSeJaExistemDados = $verifica->verificaSeAEscalaMensalEstaFinalizada($oracle, $dataSelecionadaNoFiltro, $loja);
-echo $retorno;
+// echo $retorno;
 if ($retorno === "NÃO FINALIZADA.") {
     $statusDaTabelaPosPesquisa = "NÃO FINALIZADA.";
 } else if ($retorno === "JÁ FINALIZADA.") {
@@ -26,7 +26,7 @@ if ($retorno === "NÃO FINALIZADA.") {
 
 
 $recuperacaoDedados2 = $verifica-> verificaSeOMesSelecionadoTemAlgumFuncionarioEscalado($oracle, $dataSelecionadaNoFiltro,$loja);
-ECHO $retorno1;
+// ECHO $retorno1;
 if ($retorno1 == "NÃO EXISTE CADASTRO.") {
     $statusDaTabelaPosPesquisa = "NÃO FINALIZADA.";
 }
@@ -135,17 +135,210 @@ if ($retorno1 == "NÃO EXISTE CADASTRO.") {
         ?>
     </tbody>
     
-    <input class="statusDaTabela"  id="statusDaTabelaPosPesquisa" value="<?= $statusDaTabelaPosPesquisa ?>">
+    <input class="statusDaTabela" type="hidden"  id="statusDaTabelaPosPesquisa" value="<?= $statusDaTabelaPosPesquisa ?>">
 
 </table>
-<script>
-    $(document).ready(function() {
+<script type="module" defer>
+import { criandoHtmlmensagemCarregamento, Toasty } from "../../../../base/jsGeral.js";
+$(document).ready(function() {
         var statusDaTabelaPosPesquisa = $("#statusDaTabelaPosPesquisa").val();
 
         if (statusDaTabelaPosPesquisa === "JÁ FINALIZADA.") {
+            criandoHtmlmensagemCarregamento("exibir");
+
             $('#table1').find('input, select, textarea, button').prop('disabled', true);
         }
+        criandoHtmlmensagemCarregamento("ocultar");
+
     });
+
+$('#table1').DataTable({
+    language: {
+        "sEmptyTable": "Nenhum registro encontrado",
+
+        "sInfo": " _START_ até _END_ de _TOTAL_ registros...  ",
+
+        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+
+        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+
+        "sInfoPostFix": "",
+
+        "sInfoThousands": ".",
+
+        "sLengthMenu": "_MENU_ resultados por página",
+
+        "sLoadingRecords": "Carregando...",
+
+        "sProcessing": "Processando...",
+
+        "sZeroRecords": "Nenhum registro encontrado",
+
+        "sSearch": "Pesquisar",
+
+        "oPaginate": {
+
+            "sNext": "Próximo",
+
+            "sPrevious": "Anterior",
+
+            "sFirst": "Primeiro",
+
+            "sLast": "Último"
+
+        },
+    },
+    dom: 'Bfrtip',
+    scrollY: 450,
+    scrollX: true,
+
+    scrollXInner: "100%",
+    scrollCollapse: true,
+    searching: true,
+
+    "paging": true,
+    "info": false,
+    "ordering": false,
+    "lengthMenu": [
+        [40],
+    ],
+    fixedColumns: {
+        left: 4,
+    },
+
+    buttons: [
+        // {
+        //     extend: 'excelHtml5',
+        //     exportOptions: {
+        //         columns: [0, ':visible']
+        //     }
+        // },
+        {
+            text: 'Escala Diaria',
+            className: 'btnverde',
+            action: function () {
+                window.location.href = "escalaDiaria.php";
+            }
+        },
+        {
+            text: 'Finalizar Escala',
+            className: 'btnVermelho',
+            action: function () {
+                criandoHtmlmensagemCarregamento("exibir");
+                var alteraStatusEscala = "F";
+                var usuarioLogado = $("#usuarioLogado").val();
+                var loja = $("#loja").val();
+
+                var mesPesquisa = $("#dataPesquisa").val();
+
+                var mesAtual = $("#mesAtual").val();
+
+                if (mesPesquisa == "") {
+                    mesPesquisa = mesAtual
+                };
+
+
+
+                $.ajax({
+                    url: "config/desabilita_ou_habilita_mensal.php",
+                    method: 'POST',
+                    data: "mesPesquisa=" +
+                        mesPesquisa +
+                        "&mesAtual=" +
+                        mesAtual +
+                        "&alteraStatusEscala=" +
+                        alteraStatusEscala +
+                        "&loja=" +
+                        loja +
+                        "&usuarioLogado=" +
+                        usuarioLogado,
+                    success: function (atualizaTabela1) {
+
+                        $.ajax({
+                            url: "config/pesquisar_escalaMensal.php",
+                            method: 'POST',
+                            data: 'mesPesquisa=' +
+                                mesPesquisa +
+                                "&loja=" +
+                                loja +
+                                "&usuarioLogado=" +
+                                usuarioLogado,
+                            success: function (mes_Pesquisado) {
+
+                                $('.atualizaTabela').empty().html(mes_Pesquisado);
+                                criandoHtmlmensagemCarregamento("ocultar");
+                            }
+                        });
+
+                    }
+                });
+            }
+        },
+        {
+            text: 'Liberar Escala',
+            className: 'btnVermelho',
+            action: function () {
+                criandoHtmlmensagemCarregamento("exibir");
+                var alteraStatusEscala = '';
+                var usuarioLogado = $("#usuarioLogado").val();
+                var loja = $("#loja").val();
+
+                var mesPesquisa = $("#dataPesquisa").val();
+
+                var mesAtual = $("#mesAtual").val();
+
+                if (mesPesquisa == "") {
+                    mesPesquisa = mesAtual
+                }
+
+                $.ajax({
+                    url: "config/desabilita_ou_habilita_mensal.php",
+                    method: 'POST',
+                    data: "mesPesquisa=" +
+                        mesPesquisa +
+                        "&mesAtual=" +
+                        mesAtual +
+                        "&alteraStatusEscala=" +
+                        alteraStatusEscala +
+                        "&loja=" +
+                        loja +
+                        "&usuarioLogado=" +
+                        usuarioLogado,
+                    success: function (atualizaTabela) {
+
+
+                        $.ajax({
+                            url: "config/pesquisar_escalaMensal.php",
+                            method: 'POST',
+                            data: 'mesPesquisa=' +
+                                mesPesquisa +
+                                "&loja=" +
+                                loja +
+                                "&usuarioLogado=" +
+                                usuarioLogado,
+                            success: function (mes_Pesquisado) {
+
+                                $('.atualizaTabela').empty().html(mes_Pesquisado);
+                                criandoHtmlmensagemCarregamento("ocultar");
+                            }
+                        });
+
+
+
+                    }
+                });
+            }
+        },
+    ],
+
+});
+
+
+
+</script>
+
+<script>
+ 
 </script>
 
 <Script>
@@ -175,231 +368,9 @@ if ($retorno1 == "NÃO EXISTE CADASTRO.") {
     var usuarioLogado = $("#usuarioLogado").val();
     var loja = $("#loja").val();
 
-    $('#table1').on('change', '.estilezaSelect', function() {
-        var opcaoSelecionada = $(this).val();
-        var $tr = $(this).closest('tr');
-        var funcionario = $tr.find('td.funcionario').text();
-        var matriculaFunc = $tr.find('td.matriculaFunc').text();
-        var horarioEntradaFunc = $tr.find('td.horarioEntradaFunc').text();
-        var horarioSaidaFunc = $tr.find('td.horarioSaidaFunc').text();
-        var horarioIntervaloFunc = $tr.find('td.horarioIntervaloFunc').text();
-        var cargoFunc = $tr.find('td.cargo').text();
 
-        var colIndex = $(this).closest('td').index();
-        var numeroDiaDaSemana = $('#table1 thead tr.trr th').eq(colIndex).text();
-        var mesPesquisa = $("#dataSelecionadaNoFiltro").val();
-
-        var mesAtual = $("#mesAtual").val();
-
-        if (mesPesquisa == "") {
-            mesPesquisa = mesAtual
-        }
-        // alert("mesPesquisa :  " + mesPesquisa);
-
-
-        $.ajax({
-            url: "config/insertEUpdate_EscalaMensal.php",
-            method: 'get',
-            data: 'numeroDiaDaSemana=' +
-                numeroDiaDaSemana +
-                "&opcaoSelecionada=" +
-                opcaoSelecionada +
-                "&funcionario=" +
-                funcionario +
-                "&mesAtual=" +
-                mesAtual +
-                "&mesPesquisa=" +
-                mesPesquisa +
-                "&usuarioLogado=" +
-                usuarioLogado +
-                "&matriculaFunc=" +
-                matriculaFunc +
-                "&loja=" +
-                loja +
-                "&horarioEntradaFunc=" +
-                horarioEntradaFunc +
-                "&horarioSaidaFunc=" +
-                horarioSaidaFunc +
-                "&horarioIntervaloFunc=" +
-                horarioIntervaloFunc +
-                "&cargoFunc=" +
-                cargoFunc,
-
-            // dataType: 'json',
-            success: function(retorno) {
-                console.log(retorno)
-
-
-            }
-        });
-    });
+    
 </Script>
 <script>
-    $('#table1').DataTable({
-        language: {
-            "sEmptyTable": "Nenhum registro encontrado",
 
-            "sInfo": " _START_ até _END_ de _TOTAL_ registros...  ",
-
-            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-
-            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
-
-            "sInfoPostFix": "",
-
-            "sInfoThousands": ".",
-
-            "sLengthMenu": "_MENU_ resultados por página",
-
-            "sLoadingRecords": "Carregando...",
-
-            "sProcessing": "Processando...",
-
-            "sZeroRecords": "Nenhum registro encontrado",
-
-            "sSearch": "Pesquisar",
-
-            "oPaginate": {
-
-                "sNext": "Próximo",
-
-                "sPrevious": "Anterior",
-
-                "sFirst": "Primeiro",
-
-                "sLast": "Último"
-
-            },
-        },
-        dom: 'Bfrtip',
-        scrollY: 450,
-        scrollX: true,
-
-        scrollXInner: "100%",
-        scrollCollapse: true,
-        searching: true,
-
-        "paging": true,
-        "info": false,
-        "ordering": false,
-        "lengthMenu": [
-            [40],
-        ],
-        fixedColumns: {
-            left: 2,
-        },
-
-        buttons: [{
-                text: 'Escala Diaria',
-                className: 'btnverde',
-                action: function() {
-                    window.location.href = "escalaDiaria.php";
-                }
-            },
-            {
-                text: 'Finalizar Escala',
-                className: 'btnVermelho',
-                action: function() {
-                    var alteraStatusEscala = "F";
-                    var usuarioLogado = $("#usuarioLogado").val();
-                    var loja = $("#loja").val();
-
-                    var mesPesquisa = $("#dataPesquisa").val();
-
-                    var mesAtual = $("#mesAtual").val();
-
-                    if (mesPesquisa == "") {
-                        mesPesquisa = mesAtual
-                    };
-
-
-
-                    $.ajax({
-                        url: "config/desabilita_ou_habilita_mensal.php",
-                        method: 'POST',
-                        data: "mesPesquisa=" +
-                            mesPesquisa +
-                            "&mesAtual=" +
-                            mesAtual +
-                            "&alteraStatusEscala=" +
-                            alteraStatusEscala +
-                            "&loja=" +
-                            loja +
-                            "&usuarioLogado=" +
-                            usuarioLogado,
-                        success: function(atualizaTabela) {
-                            $.ajax({
-                                url: "config/pesquisar_escalaMensal.php",
-                                method: 'POST',
-                                data: 'mesPesquisa=' +
-                                    mesPesquisa +
-                                    "&loja=" +
-                                    loja +
-                                    "&usuarioLogado=" +
-                                    usuarioLogado,
-                                success: function(mes_Pesquisado) {
-
-                                    $('.atualizaTabela').empty().html(mes_Pesquisado);
-
-                                }
-                            });
-
-
-
-                        }
-                    });
-                }
-            },
-            {
-                text: 'Liberar Escala',
-                className: 'btnVermelho',
-                action: function() {
-                    var alteraStatusEscala = '';
-                    var usuarioLogado = $("#usuarioLogado").val();
-                    var loja = $("#loja").val();
-
-                    var mesPesquisa = $("#dataPesquisa").val();
-
-                    var mesAtual = $("#mesAtual").val();
-
-                    if (mesPesquisa == "") {
-                        mesPesquisa = mesAtual
-                    }
-                    $.ajax({
-                        url: "config/desabilita_ou_habilita_mensal.php",
-                        method: 'POST',
-                        data: "mesPesquisa=" +
-                            mesPesquisa +
-                            "&mesAtual=" +
-                            mesAtual +
-                            "&alteraStatusEscala=" +
-                            alteraStatusEscala +
-                            "&loja=" +
-                            loja +
-                            "&usuarioLogado=" +
-                            usuarioLogado,
-                        success: function(atualizaTabela3) {
-                            $.ajax({
-                                url: "config/pesquisar_escalaMensal.php",
-                                method: 'POST',
-                                data: 'mesPesquisa=' +
-                                    mesPesquisa +
-                                    "&loja=" +
-                                    loja +
-                                    "&usuarioLogado=" +
-                                    usuarioLogado,
-                                success: function(mes_Pesquisado) {
-
-                                    $('.atualizaTabela').empty().html(mes_Pesquisado);
-
-                                }
-                            });
-                        }
-                    });
-                }
-            }
-
-        ],
-
-    });
 </script>
